1c86c2c7f1a06b3cc8e3f823a48afafd6ecac636
gasampaiosouza <gasampaio.souza@gmail.com>
2020-06-30 22:10:16 -0300

    initial commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import List from '../components/List/List';
+// import List from '../components/List/List';
 import Header from '../components/Header/Header';
 
 const Index = () => (
   <div className="container">
     <Header />
-    <List />
+    {/* <List /> */}
   </div>
 );
 
