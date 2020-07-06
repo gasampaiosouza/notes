b2ede7b8a9a2a75905fc52573a51a3d6412d06ec
gasampaiosouza <gasampaio.souza@gmail.com>
2020-07-06 13:21:03 -0300

    add search feature

--- components/List/index.js
+++ components/List/index.js
@@ -2,6 +2,9 @@ import style from './list.module.scss';
 import toggleNote from './toggleNote';
 import { useRef, useState } from 'react';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
 function List({ content, setContent }) {
   const listItem = useRef(null);
   const isListClear = Object.keys(content).length !== 1;
@@ -30,15 +33,23 @@ function List({ content, setContent }) {
       : 0;
   };
 
+  const filterFunction = (item) =>
+    item.title.toLowerCase().includes(searchValue.trim().toLowerCase());
+
   return (
     <div className={style['list']}>
       <h1 className={style['list--title']}>Your notes</h1>
 
       <div className={style['list--search']}>
+        <span className={style['list--search__icon']}>
+          <FontAwesomeIcon icon={faSearch} />
+        </span>
+
         <input
           type="text"
           className={style['list--search__input']}
           onChange={search}
+          placeholder="Search..."
         />
       </div>
 
@@ -49,37 +60,35 @@ function List({ content, setContent }) {
 
       {isListClear ? (
         (localStorage.setItem('listItems', JSON.stringify(content)),
-        content
-          .filter((item) => item.title.includes(searchValue))
-          .map(({ title, id, desc, date }) => {
-            if (!title) return;
+        content.filter(filterFunction).map(({ title, id, desc, date }) => {
+          if (!title) return;
 
-            return (
-              <div
-                className={`${style['list--item']}`}
-                onClick={toggleNote}
-                key={id}
-                style={{ maxHeight: '50px' }}
-                ref={listItem}
-                id={id.toString()}
-                onContextMenu={(ev) => {
-                  ev.preventDefault();
-                  const clickedListID = ev.currentTarget.id;
+          return (
+            <div
+              className={`${style['list--item']}`}
+              onClick={toggleNote}
+              key={id}
+              style={{ maxHeight: '50px' }}
+              ref={listItem}
+              id={id.toString()}
+              onContextMenu={(ev) => {
+                ev.preventDefault();
+                const clickedListID = ev.currentTarget.id;
 
-                  deleteNote(clickedListID);
-                }}
-              >
-                <div className={style['list--item__info']}>
-                  <p className={style['list--item__title']}>{title}</p>
-                  <p className={style['list--item__date']}>{date}</p>
-                </div>
-
-                <div className={style['list--item__note']}>
-                  <p>{desc}</p>
-                </div>
+                deleteNote(clickedListID);
+              }}
+            >
+              <div className={style['list--item__info']}>
+                <p className={style['list--item__title']}>{title}</p>
+                <p className={style['list--item__date']}>{date}</p>
               </div>
-            );
-          }))
+
+              <div className={style['list--item__note']}>
+                <p>{desc}</p>
+              </div>
+            </div>
+          );
+        }))
       ) : (
         <p className={`${style['list--empty__message']} desc`}>
           You do not have any notes
